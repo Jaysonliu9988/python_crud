3ace771728ec2cd0b7a687dd3ea881af02be6a79
jayson <leobest9688@gmail.com>
2021-10-31 23:24:17 +0800

    add the input the function

--- admin.py
+++ admin.py
@@ -1,4 +1,20 @@
 import json
 
-def input_int(promot):
-    pass
+# This function repeatedly prompts for input until an integer is entered.
+def input_int(prompt):
+    while True:
+        target = input(prompt)
+        try:
+            return int(target)
+        except:
+            print('Need to enter an integer.')
+            continue
+
+def input_something(prompt):
+    while True:
+        char_target = input(prompt).strip()
+        if not char_target:
+            print('Enter a valid string, which cannot be composed of blank keys and tabs.')
+        else:
+            return char_target
+
